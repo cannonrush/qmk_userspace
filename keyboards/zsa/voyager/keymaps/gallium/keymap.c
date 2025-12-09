0ae13805452f54bbff9b3b5e5503a5b7bb9ed2f6
Nikita Vinogradov <46093925+cannonrush@users.noreply.github.com>
2025-12-09 14:43:22 +1100

    Configure RGB lighting (gallium)

--- keyboards/zsa/voyager/keymaps/gallium/keymap.c
+++ keyboards/zsa/voyager/keymaps/gallium/keymap.c
@@ -16,9 +16,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,             KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,             KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
         CW_TOGG,  KC_B,     KC_L,     KC_D,     KC_C,     KC_V,             KC_J,     KC_Y,     KC_O,     KC_U,     KC_COMMA, KC_BSLS,
-        SFT_T(KC_BSPC), HOME_N, HOME_R, HOME_T, HOME_S,   KC_G,             KC_P,     HOME_H,   HOME_A,   HOME_E,   HOME_I,   RSFT_T(KC_QUOT),
+        SFT_T(KC_BSPC), KC_N, KC_R,   KC_T,     KC_S,     KC_G,             KC_P,     KC_H,     KC_A,     KC_E,     KC_I,     RSFT_T(KC_QUOT),
         KC_LGUI,  ALT_T(KC_X),KC_Q,   KC_M,     KC_W,     KC_Z,             KC_K,     KC_F,     KC_QUOT,  KC_SCLN,  RALT_T(KC_DOT),KC_RCTL,
                                                 LT(1,KC_TAB), KC_SPC,       KC_BSPC,  LT(2,KC_ENT)
     ),
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RM_TOGG,  QK_KB,    RM_NEXT,  RGB_M_P,  RM_VALD,  RM_VALU,          _______,  _______,  _______,  _______,  _______,  QK_BOOT,
         _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,          KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   _______,  _______,
         _______,  KC_MPRV,  KC_MNXT,  KC_MSTP,  KC_MPLY,  _______,          KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,          _______,  C(S(KC_TAB)),C(KC_TAB),_______,_______, _______,
+        _______,  _______,  _______,  _______,  RM_HUED,  RM_HUEU,          _______,  C(S(KC_TAB)),C(KC_TAB),_______,_______, _______,
                                                 _______,  _______,          _______,  _______
     ),
     // [3] = LAYOUT(
